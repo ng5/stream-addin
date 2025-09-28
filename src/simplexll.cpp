7aa2f20ef8ffcaab78ed5ac52a13afc083c9e396
Rohit Vishwakarma <inbox.rohitv@gmail.com>
2025-09-28 13:13:11 +0100

    Initial Commit

---
+++ src/simplexll.cpp
@@ -0,0 +1,20 @@
+#include <exception>
+#include <windows.h>
+#include "xlcall.h"
+#include "framewrk.h"
+
+extern "C" __declspec(dllexport) double WINAPI CalcCircum(const double x) { return x * 6.283185308; }
+extern "C" __declspec(dllexport) int __stdcall xlAutoOpen(void) {
+    try {
+        static XLOPER12 xDLL{};
+        Excel12f(xlGetName, &xDLL, 0);
+        int rc = Excel12f(xlfRegister, 0, 4, (LPXLOPER12) &xDLL, (LPXLOPER12) TempStr12(L"CalcCircum"),
+                          (LPXLOPER12) TempStr12(L"BB"), (LPXLOPER12) TempStr12(L"CalcCircum"));
+        Excel12f(xlFree, nullptr, 1, static_cast<LPXLOPER12>(&xDLL));
+        return 1;
+    } catch (const std::exception &e) {
+        return 0;
+    }
+}
+
+extern "C" __declspec(dllexport) int __stdcall xlAutoClose(void) { return 1; }
